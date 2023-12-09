df86ef77b0200b24d0f50283e06679594ae12531
Puran Singh <puran@Purans-MacBook-Pro.local>
2023-12-08 20:26:25 -0800

    rust version

--- rust/target/debug/deps/rust-51e7b74d774c9698.d
+++
@@ -1,6 +0,0 @@
-/Users/puran/projects/personal/monkey/rust/target/debug/deps/librust-51e7b74d774c9698.rmeta: src/main.rs src/monkey.rs
-
-/Users/puran/projects/personal/monkey/rust/target/debug/deps/rust-51e7b74d774c9698.d: src/main.rs src/monkey.rs
-
-src/main.rs:
-src/monkey.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/Users/puran/projects/personal/monkey/rust/target/debug/rust: /Users/puran/projects/personal/monkey/rust/src/main.rs /Users/puran/projects/personal/monkey/rust/src/monkey.rs
